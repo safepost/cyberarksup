acfd67406f9c3a82e933cfb3ef0a3dde4f982878
gotz <gotz@pm.me>
2020-03-20 15:01:05 +0100

    check vault first

--- src/check_status.go
+++ src/check_status.go
@@ -6,6 +6,21 @@ func status(config FinalConfig) bool {
 
 	logrus.Debug("Starting health tests...")
 
+	// Check Vault
+	vaultConn := checkVault(config.vaultIPs)
+	if !vaultConn {
+		logrus.Info("Connexion to Vaults failed !")
+		return false
+	}
+
+	// Check Services
+	serviceStatus := checkServices(config.services)
+	if !serviceStatus {
+		logrus.Info("Service check failed !")
+		return false
+	}
+
+	// Check Disks
 	for _, disk := range config.disks {
 		diskStatus := DiskUsage(disk)
 		logrus.Debug(diskStatus)
@@ -13,18 +28,6 @@ func status(config FinalConfig) bool {
 			logrus.Info("Disk " + disk + " available space is less than 10% !")
 			return false
 		}
-	}
-
-	vaultConn := checkVault(config.vaultIPs)
-	if !vaultConn {
-		logrus.Info("Connexion to Vaults failed !")
-		return false
-	}
-
-	serviceStatus := checkServices(config.services)
-	if !serviceStatus {
-		logrus.Info("Service check failed !")
-		return false
 	}
 
 	logrus.Debug("All checks went well !")
